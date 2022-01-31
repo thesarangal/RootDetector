dd26b07fd17fea51af34f3c73b3fae72305800c7
Sarangal <46309117+thesarangal@users.noreply.github.com>
2022-01-31 18:35:13 +0530

    RootDetector Class Added

---
+++ RootDetector.kt
@@ -0,0 +1,110 @@
+package com.relinns.botpenguin.chatapp.utils
+
+import android.os.Build
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+
+/**
+ * Class with Root Detection Functions
+ *
+ * @author Rajat Sarangal
+ * @since January 31, 2022
+ * @link https://github.com/thesarangal/RootDetector
+ * */
+class RootDetector {
+
+    /**
+     * @return TRUE if Any Test for Root Detection Pass out
+     * */
+    fun isDeviceRooted(): Boolean {
+        return isSuperUserExists() ||
+                detectTestKeys() ||
+                checkSuExists() ||
+                checkBinaries()
+    }
+
+    /**
+     * Check Test Keys Available
+     *
+     * TRUE: Mean No Genuine OS or Using Custom OS
+     * */
+    private fun detectTestKeys(): Boolean {
+        val buildTags = Build.TAGS
+        return buildTags != null && buildTags.contains("test-keys")
+    }
+
+    /**
+     * Check Binary Existence
+     * */
+    private fun checkBinaries(): Boolean {
+        return findBinary("su") || findBinary("busybox")
+    }
+
+    /**
+     * Check Directories
+     *
+     * @param binaryName Root Related File Name
+     * */
+    private fun findBinary(binaryName: String): Boolean {
+        var found = false
+        if (!found) {
+            val places = arrayOf(
+                "/sbin/",
+                "/system/bin/",
+                "/system/xbin/",
+                "/data/local/xbin/",
+                "/data/local/bin/",
+                "/system/sd/xbin/",
+                "/system/bin/failsafe/",
+                "/data/local/",
+                "/data/local/bin/",
+                "/data/local/xbin/",
+                "/su/bin/",
+                "/system/bin/.ext/",
+                "/system/sd/xbin/",
+                "/system/usr/we-need-root/",
+                "/system/xbin/",
+                "/system/app/Superuser.apk",
+                "/cache",
+                "/data",
+                "/dev"
+            )
+            for (where in places) {
+                if (File(where + binaryName).exists()) {
+                    found = true
+                    break
+                }
+            }
+        }
+        return found
+    }
+
+    /**
+     * Check is SuperUser.apk Exists
+     * */
+    private fun isSuperUserExists(): Boolean {
+        val file = File("/system/app/Superuser.apk")
+        return file.exists()
+    }
+
+    /**
+     * Checking for SU
+     * @return TRUE if SU exists
+     */
+    private fun checkSuExists(): Boolean {
+        var process: Process? = null
+        return try {
+            process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
+            val `in` = BufferedReader(
+                InputStreamReader(process.inputStream)
+            )
+            `in`.readLine()
+            process.destroy()
+            true
+        } catch (e: java.lang.Exception) {
+            process?.destroy()
+            false
+        }
+    }
+}
