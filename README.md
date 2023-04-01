4c9e83eaacd8aaaae5c4b886438e5f111f688ee7
Rajat Kumar <rajatkumar>
2023-04-01 20:29:57 +0530

    - [Added] checkForDangerousProps() Function - [Update] README.md

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# RootDetector
-Class with Root Detection Function
+# Root Detector
+
+Root Detector is a Kotlin-based library that detects whether an Android device has been rooted or
+not. The library checks for various root indicators such as the presence of root-related binary
+files, system properties, and superuser.apk file, etc.
+
+## Usage
+
+To use the Root Detector library, simply call the isDeviceRooted() method provided by the
+RootDetector object. The method returns a boolean value indicating whether the device is rooted or
+not. Here is an example of how to use the library:
+
+```bash
+if (RootDetector.isDeviceRooted()) {
+    // Device is rooted
+} else {
+    // Device is not rooted
+}
+```
+
+## Detection Techniques
+
+The Root Detector library uses the following techniques to detect root access on an Android device:
+
+- Checking for the presence of root-related binary files such as su, busybox, and magisk in common
+  system directories.
+- Checking for the presence of the Superuser.apk file in the system directory.
+- Checking for the existence of potentially dangerous system properties.
+- Using the which command to locate the su binary file in the system's path.
+- Detecting whether the device is using test keys or not.
+
+## Contributions
+
+Contributions to the Root Detector library are welcome. If you find a bug or have a feature request,
+please open an issue on the [GitHub repository](https://github.com/thesarangal/RootDetector).
+
+## License
+
+The Root Detector library is released under the [MIT License](https://opensource.org/licenses/MIT).
--- RootDetector.kt
+++ RootDetector.kt
@@ -7,94 +7,88 @@ import java.io.InputStreamReader
  * Class with Root Detection Functions
  *
  * @author Rajat Sarangal
- * @since January 31, 2022
+ * @since April 01, 2023
  * @link https://github.com/thesarangal/RootDetector
  * */
-class RootDetector {
+object RootDetector {
 
     /**
      * @return TRUE if Any Test for Root Detection Pass out otherwise FALSE
      * */
     fun isDeviceRooted(): Boolean {
-        return isSuperUserExists() ||
-                detectTestKeys() ||
-                checkSuExists() ||
-                checkBinaries()
+        return isSuperUserExists() || detectTestKeys() || checkSuExists() || checkBinaries() ||
+                checkForDangerousProps()
     }
 
     /**
-     * Check Test Keys Available
-     *
-     * @return TRUE Mean No Genuine OS or Using Custom OS otherwise FALSE
-     * */
+     * Detects if the device is using test keys. If the device is using test keys,
+     * it is likely that the operating system is not a genuine version or is a custom version.
+     * @return true if the device is using test keys, otherwise false.
+     */
     private fun detectTestKeys(): Boolean {
         val buildTags = Build.TAGS
         return buildTags != null && buildTags.contains("test-keys")
     }
 
     /**
-     * Check Binary Existence
-     * 
-     * @return TRUE if Root related binary files found otherwise FALSE
-     * */
+     * Check for Root Related Binary Files
+     *
+     * @return TRUE if any root related binary files such as "su", "busybox", or "magisk" exist in common system directories, otherwise FALSE
+     */
     private fun checkBinaries(): Boolean {
-        return findBinary("su") || findBinary("busybox")
+        return findBinary("su") || findBinary("busybox") || findBinary("magisk")
     }
 
     /**
-     * Check Directories
-     *
-     * @param binaryName Root Related File Name
-     * @return TRUE if Root related binary files found otherwise FALSE
-     * */
+     * Check if a root-related binary file exists in any of the commonly used root directories.
+     * @param binaryName The name of the root-related binary file to search for.
+     * @return true if the binary file is found in any of the common root directories, otherwise false.
+     */
     private fun findBinary(binaryName: String): Boolean {
-        var found = false
-        if (!found) {
-            val places = arrayOf(
-                "/sbin/",
-                "/system/bin/",
-                "/system/xbin/",
-                "/data/local/xbin/",
-                "/data/local/bin/",
-                "/system/sd/xbin/",
-                "/system/bin/failsafe/",
-                "/data/local/",
-                "/data/local/bin/",
-                "/data/local/xbin/",
-                "/su/bin/",
-                "/system/bin/.ext/",
-                "/system/sd/xbin/",
-                "/system/usr/we-need-root/",
-                "/system/xbin/",
-                "/system/app/Superuser.apk",
-                "/cache",
-                "/data",
-                "/dev"
-            )
-            for (where in places) {
-                if (File(where + binaryName).exists()) {
-                    found = true
-                    break
-                }
+        val places = arrayOf(
+            "/sbin/",
+            "/system/bin/",
+            "/system/xbin/",
+            "/data/local/xbin/",
+            "/data/local/bin/",
+            "/system/sd/xbin/",
+            "/system/bin/failsafe/",
+            "/data/local/",
+            "/data/local/bin/",
+            "/data/local/xbin/",
+            "/su/bin/",
+            "/system/bin/.ext/",
+            "/system/sd/xbin/",
+            "/system/usr/we-need-root/",
+            "/system/xbin/",
+            "/system/app/Superuser.apk",
+            "/cache",
+            "/data",
+            "/dev"
+        )
+        for (where in places) {
+            if (File(where + binaryName).exists()) {
+                return true
+
             }
         }
-        return found
+        return false
     }
 
     /**
-     * Check is SuperUser.apk Exists
-     * 
-     * @returnTRUE if Superuser app file found otherwise FALSE
-     * */
+     * Checks if the Superuser.apk file exists in the system directory.
+     * @return true if the Superuser.apk file exists, false otherwise
+     */
     private fun isSuperUserExists(): Boolean {
         val file = File("/system/app/Superuser.apk")
         return file.exists()
     }
 
     /**
-     * Checking for SU
-     *
-     * @return TRUE if SU exists
+     * This function checks whether the 'su' binary file exists in the system's path, which is an indicator of
+     * the presence of root access. It uses the 'which' command to locate the 'su' binary file.
+     * @return a boolean value indicating whether the 'su' binary file exists in the system's path or not.
+     * If the binary exists, the function returns true; otherwise, it returns false.
      */
     private fun checkSuExists(): Boolean {
         var process: Process? = null
@@ -110,5 +104,53 @@ class RootDetector {
             process?.destroy()
             false
         }
+    }
+
+    /**
+     * Checks for the existence of potentially dangerous system properties.
+     * @return true if any of the dangerous properties are found, false otherwise.
+     */
+    private fun checkForDangerousProps(): Boolean {
+        val props = arrayOf(
+            "[ro.debuggable]",
+            "[ro.build.selinux]",
+            "[ro.kernel.android.checkjni]",
+            "[ro.kernel.android.qemud]",
+            "[ro.secure]",
+            "[ro.allow.mock.location]",
+            "[service.adb.root]",
+            "[persist.service.adb.enable]",
+            "[ro.debug.buildsys.systrace]",
+            "[persist.sys.root_access]",
+            "[debug.firebase.analytics.app]",
+            "[dalvik.vm.checkjni]"
+        )
+        for (prop in props) {
+            val value = getProperty(prop)
+            if (value.isNotEmpty()) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    /**
+     * Returns the value of a system property.
+     * @param property The name of the system property to retrieve.
+     * @return The value of the system property, or an empty string if it is not set or an error occurs.
+     */
+    private fun getProperty(property: String): String {
+        var value = ""
+        try {
+            val process = Runtime.getRuntime().exec("getprop $property")
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            value = reader.readLine().trim()
+            reader.close()
+            process.destroy()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return value
     }
 }
